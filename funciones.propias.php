0176c48c32b5e846e004a016ac2612389e8b88d5
brirontani <brirontani@gmail.com>
2026-09-06 11:44:19 -0300

    Add files via upload

--- funciones.propias.php
+++ funciones.propias.php
@@ -14,3 +14,10 @@ function saludo2($nombre){
     return "mi nombre es: $nombre";
 }
     echo saludo2("lara");
+
+function promedio_alumno($nota1, $nota2, $nota3){
+    $promedio = ($nota1 + $nota2 + $nota3) / 3;
+    return $promedio;
+}
+$promedio=promedio_alumno(8,9,4);
+echo "el promedio es: ".$promedio;
