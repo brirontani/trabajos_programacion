5ede04619a69b729111994eeb58494c4745ae5d7
Brisa Rontani <brirontani@gmail.com>
2026-09-07 19:29:07 -0300

    Add files via upload

---
+++ funcion.string.contar.php
@@ -0,0 +1,8 @@
+<?php
+
+$cadena_texto="hola este es mi mundo";
+$longitud=strlen($cadena_texto);
+echo $cadena_texto." tiene ".$longitud." caracteres <br>";
+
+$palabras=str_word_count($cadena_texto);
+echo $cadena_texto." tiene ".$palabras." palabras <br>";
