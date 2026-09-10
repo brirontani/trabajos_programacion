1c4e0843474f0c6fc4cf938a16f8ac85187ea1ba
Brisa Rontani <brirontani@gmail.com>
2026-09-10 11:43:07 -0300

    Add files via upload

---
+++ ciclos.php
@@ -0,0 +1,22 @@
+<?php
+#$c=1;
+
+#while en cremento
+#while($c<=20){
+#echo $c."<br>";
+#$c++;
+#}
+
+#while en decremento
+#while($c>=1){
+#echo $c."<br>";
+#$c--;
+#}
+
+$c=1;
+$num=7;
+
+while($c<=12){
+    echo $num*$c."<br>";
+    $c++;
+}
---
+++ dowhile.php
@@ -0,0 +1,19 @@
+<?php
+#$c=1;
+#do{
+   # echo $c."<br>";
+   # $c++;
+#}while($c<=20);
+
+#$c=20;
+#do{
+    #echo $c."<br>";
+    #$c--;
+#}while($c>=1);
+
+$c=1;
+$num=5;
+do{
+    echo $c*$num."<br>";
+    $c++;
+}while($c<=12);
