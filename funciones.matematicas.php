ee81c1c3557bd11ae740bb4a010ce085c9a1c8b1
Brisa Rontani <brirontani@gmail.com>
2026-09-13 20:16:28 -0300

    Add files via upload

---
+++ funciones.matematicas.php
@@ -0,0 +1,15 @@
+<?php
+
+echo pow(6,2);
+echo "<br>";
+echo sqrt(36);
+echo "<br>";
+echo rand(1,80);
+echo "<br>";
+echo pi();
+echo "<br>";
+echo floor(4.5);
+echo "<br>";
+echo ceil(4.5);
+echo "<br>";
+echo round(23.34);
