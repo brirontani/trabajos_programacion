ae86017019accaac9fa2306d9ab94dbace918b97
Brisa Rontani <brirontani@gmail.com>
2026-09-14 19:08:15 -0300

    Add files via upload

---
+++ fecha.php
@@ -0,0 +1,73 @@
+<?php
+date_default_timezone_set("america/costa_rica");
+function fecha_espanol_larga(){
+   $fecha_dia=date("d");
+    $fecha_mes=date("m");
+    $fecha_year=date("Y");
+
+    $dia_semana=[
+        "Monday" => "Lunes",
+        "Tuesday" => "Martes",
+        "Wednesday" => "Miércoles",
+        "Thursday" => "Jueves",
+        "Friday" => "Viernes",
+        "Saturday" => "Sábado",
+        "Sunday" => "Domingo"
+    ];
+
+    $meses_year=[
+        "01" => "Enero",
+        "02" => "Febrero",
+        "03" => "Marzo",
+        "04" => "Abril",
+        "05" => "Mayo",
+        "06" => "Junio",
+        "07" => "Julio",
+        "08" => "Agosto",
+        "09" => "Septiembre",
+        "10" => "Octubre",
+        "11" => "Noviembre",
+        "12" => "Diciembre"
+    ];
+
+$fecha_final=$dia_semana[date("l")]." ".$fecha_dia." de ".$meses_year[$fecha_mes]." del ".$fecha_year;
+    return $fecha_final;
+}
+echo fecha_espanol_larga();
+
+echo "<br>";
+
+function fecha_espanol_corta($fecha=" "){
+    if($fecha==""){
+$fecha=date("d-m-Y");
+    }else{
+     $fecha=date("d-m-Y", strtotime($fecha));   
+    }
+    $fecha=explode("-",$fecha);
+
+
+
+   $fecha_dia=$fecha[0];
+    $fecha_mes=$fecha[1];
+    $fecha_year=$fecha[2];
+
+    $meses_year=[
+        "01" => "Enero",
+        "02" => "Febrero",
+        "03" => "Marzo",
+        "04" => "Abril",
+        "05" => "Mayo",
+        "06" => "Junio",
+        "07" => "Julio",
+        "08" => "Agosto",
+        "09" => "Septiembre",
+        "10" => "Octubre",
+        "11" => "Noviembre",
+        "12" => "Diciembre"
+    ];
+
+
+$fecha_final=$fecha_dia." de ".$meses_year[$fecha_mes]." del ".$fecha_year;
+    return $fecha_final;
+    }
+    echo fecha_espanol_corta("2026");
